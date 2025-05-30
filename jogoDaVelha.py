07657fc5b2ca12b8a90674aa231b3065b87927e6
Janitor Prates <janitorprates@gmail.com>
2025-05-30 00:23:48 -0300

    Jogo da velha parte 2

--- jogoDaVelha.py
+++ jogoDaVelha.py
@@ -1,11 +1,4 @@
-"""
 
-tabuleiro = [
-    ['a', 'b', 'c'],
-    ['d', 'e', 'f'],
-    ['g', 'h', 'i']
-]
-"""
 tabuleiro = [[' ' for _ in range(3)] for _ in range(3)]
 jogador = 'X'
 
@@ -13,19 +6,31 @@ def exibeTabuleiro():
     for linha in tabuleiro:
        print('|'.join(linha))
        print('-'*6)
-       #print(f'{linha[0]}|{linha[1]}|{linha[2]}')
 
 def jogada(linha, coluna):
+    if (
+        not 0 <= linha <= 2 or
+        not 0 <= coluna <= 2 or 
+        tabuleiro[linha][coluna] != ' '
+    ):
+        print('Jogada inválida!')
+        return jogador
+    
     tabuleiro[linha][coluna] = jogador
-    if jogador == 'X':
-        return 'O'
-    else:
-        return 'X'
+    return 'O' if jogador == 'X' else 'X'
 
-jogada(1,1)
-jogada(2,1)
-jogada(1,0)
+while True:
+    print(f'Jogador da vez: {jogador}')
+    try:
+        linha = int(input('Digite a linha: '))
+        coluna = int(input('Digite a coluna: '))
+        jogador = jogada(linha, coluna)
+    except IndexError:
+        print('Digite valores numéricos entre 0 e 2')
+    except ValueError:
+        print('Os valores devem ser números inteiros')
 
 
-exibeTabuleiro()
+    exibeTabuleiro()
+
 
