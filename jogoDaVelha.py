1f92c1f11c044668468856845899db9141b4bc9b
Janitor Prates <janitorprates@gmail.com>
2025-05-30 00:52:58 -0300

    Jogo da velha parte 3 final

--- jogoDaVelha.py
+++ jogoDaVelha.py
@@ -19,6 +19,55 @@ def jogada(linha, coluna):
     tabuleiro[linha][coluna] = jogador
     return 'O' if jogador == 'X' else 'X'
 
+def temGanhador():
+    """ Verifica as linhas"""
+    if (
+        tabuleiro[0][0] != ' ' and
+        tabuleiro[0][0] == tabuleiro[0][1] and
+        tabuleiro[0][0] == tabuleiro[0][2]
+    ):
+        print(f'{tabuleiro[0][0]} GANHOU!!!!')
+        return True
+    
+    """ Verifica as colunas"""
+    if (
+        tabuleiro[0][coluna] != ' ' and
+        tabuleiro[0][coluna] == tabuleiro[1][coluna] and
+        tabuleiro[0][coluna] == tabuleiro[2][coluna]
+    ):
+        print(f'{tabuleiro[0][coluna]} GANHOU!!!!')
+        return True
+
+
+    """Verifica as diagonais"""
+    if(
+        tabuleiro[1][1] != ' ' and
+        (
+            (
+                tabuleiro[0][0] == tabuleiro[1][1] and
+                tabuleiro[0][0] == tabuleiro[2][2]
+            ) or
+            (
+                tabuleiro[0][2] == tabuleiro[1][1] and
+                tabuleiro[1][1] == tabuleiro[2][0]
+            )
+        )
+        
+    ):
+        print(f'{tabuleiro[1][1]} GANHOU!!!!')
+        return True
+    """ Se não teve ganhador em nenhuma forma"""
+    return False
+
+
+def temEmpate():
+    for linha in range(3):
+        for coluna in range(3):
+            if tabuleiro[linha][coluna] == ' ':
+                return False
+    print('Acabou empatado!!!')
+    return True        
+
 while True:
     print(f'Jogador da vez: {jogador}')
     try:
@@ -30,7 +79,10 @@ while True:
     except ValueError:
         print('Os valores devem ser números inteiros')
 
-
     exibeTabuleiro()
+    if temGanhador() or temEmpate():
+        break
+    
+
 
 
